f52687f7e7a30786479b8119c63c18b866474967
Giordon Stark <kratsg@gmail.com>
2017-12-10 21:35:09 -0600

    update auth0 variables

--- auth0-variables.js
+++ auth0-variables.js
@@ -1,3 +1,3 @@
-var AUTH0_CLIENT_ID='lyoXwJerzwvG7GJwnlHcwtZUN0ur8pAE';
-var AUTH0_DOMAIN='signsfive-api.auth0.com';
+var AUTH0_CLIENT_ID='MNZGv1cpercLQgy0fCb3MHqeO0HN8uGP';
+var AUTH0_DOMAIN='signsfive.auth0.com';
 var AUTH0_CALLBACK_URL=location.href;
